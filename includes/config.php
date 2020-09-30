41b6f10e90344cbcbd458629448cfad5e0991023
Chege Bryan <cbbryan8@gmail.com>
2020-09-30 19:22:05 +0300

    Get database information from environment variables

--- includes/config.php
+++ includes/config.php
@@ -1,6 +1,11 @@
 <?php
 
-$conn = new mysqli("localhost", "root", "", "id13529731_ccmsys");
+$host = getenv('DB_HOST');
+$user = getenv('DB_USERNAME');
+$password = getenv('DB_PASSWORD');
+$database = getenv('DB_DATABASE');
+
+$conn = new mysqli($host, $user, $password, $database);
 if ($conn->connect_error) {
   echo "Database Connection Failed";
 }
