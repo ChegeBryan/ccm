67645ec4844ef50cdb323f5024ea152260637360
Chege Bryan <cbbryan8@gmail.com>
2020-10-03 09:15:12 +0300

    Update approval status check

--- advisor/not_approved.php
+++ advisor/not_approved.php
@@ -31,9 +31,16 @@ require_once '../includes/config.php';
       <div class="container-fluid pt-9">
 
         <!-- Page Heading -->
-        <div class="d-sm-flex align-items-center mb-4">
-          <h1 class="h3 mb-0 text-secondary">You are not yet approved by the site administrator. Once Approved you will
-            able to view this page.</h1>
+        <div class="d-sm-flex align-items-center justify-content-center mb-4">
+          <div class="alert alert-info" role="alert">
+            <h4 class="alert-heading">Approval Status</h4>
+            <p>You are not yet approved by the site administrator. Once Approved you will
+              able to view this page. And interact fully with the platform.</p>
+            <hr>
+            <p class="mb-0">If already approved, update status by ending this session <a href="#" class="alert-link"
+                 data-toggle="modal" data-target="#logoutModal">Here</a>
+              again.</p>
+          </div>
         </div>
       </div>
       <!-- /.container-fluid -->
