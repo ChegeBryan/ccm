1ccb3121d5e3792ca3f75f46d44fc5491071f96c
Chege Bryan <cbbryan8@gmail.com>
2020-10-05 11:48:01 +0300

    Add total cost to pay on saving appointments

--- farmer/book_appointment.php
+++ farmer/book_appointment.php
@@ -5,14 +5,34 @@ require_once "../includes/config.php";
 
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $sql = "INSERT INTO ccm_appointments (made_by, farm_input, quantity, pick_date) VALUES (?, ?, ?, ?)";
+  $cost = "SELECT cost FROM ccm_farm_inputs WHERE id=?";
+
+  if ($stmt = $conn->prepare($cost)) {
+
+    $stmt->bind_param("i", $param_id);
+    $param_id = $_POST["farm_input"];
+    if ($stmt->execute()) {
+      $result = $stmt->get_result();
+
+      if ($result->num_rows == 1) {
+        $row = $result->fetch_array(MYSQLI_ASSOC);
+        $cost_per_kg = $row['cost'];
+      } else {
+        exit();
+      }
+    } else {
+      header('location: ../error.php');
+    }
+  }
+  $sql = "INSERT INTO ccm_appointments (made_by, farm_input, quantity, total_cost, pick_date) VALUES (?, ?, ?, ?, ?)";
 
   if ($stmt = $conn->prepare($sql)) {
-    $stmt->bind_param("iiis", $param_farmer, $param_farm_input, $param_quantity, $param_date);
+    $stmt->bind_param("iiids", $param_farmer, $param_farm_input, $param_quantity, $param_total_cost, $param_date);
 
     $param_farmer = $_GET["farmer"];
     $param_farm_input = intval($_POST["farm_input"]);
     $param_quantity = intval($_POST["quantity"]);
+    $param_total_cost = intval($_POST["quantity"]) * $cost_per_kg;
     $param_date = trim($_POST["p_date"]);
 
     if ($stmt->execute()) {
