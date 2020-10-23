084c7baa874344e0ea9fbc52f01379556b0646a2
Chege Bryan <cbbryan8@gmail.com>
2020-10-23 18:45:09 +0300

    Add user type checkbox

--- passwordRecovery/email.php
+++ passwordRecovery/email.php
@@ -29,12 +29,27 @@
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" class="needs-validation" method="POST"
               novalidate>
           <div class="form-group <?php echo (!empty($email_err)) ? 'has-error' : ''; ?>">
-            <label for="email"><b>Email</b></label>
+            <label for="email" class="text-secondary">Email</label>
             <input type="email" class="form-control" id="email" placeholder="Enter your email" name="email"
                    value="<?php echo $email; ?>" required>
             <span class="form-text text-danger"><small><?php echo $email_err; ?></small></span>
           </div>
-          <button type="submit" class="btn btn-danger btn-block">Submit</button>
+
+          <p class="form-text text-secondary">Select User Type</p>
+
+          <div class="custom-control custom-radio custom-control-inline">
+            <input type="radio" id="usertype1" name="usertype" class="custom-control-input" checked value="farmer">
+            <label class="custom-control-label" for="usertype1">Farmer</label>
+          </div>
+          <div class="custom-control custom-radio custom-control-inline">
+            <input type="radio" id="usertype2" name="usertype" class="custom-control-input" value="advisor">
+            <label class="custom-control-label" for="usertype2">Advisor</label>
+          </div>
+          <div class="custom-control custom-radio custom-control-inline">
+            <input type="radio" id="usertype3" name="usertype" class="custom-control-input" value="staff">
+            <label class="custom-control-label" for="usertype3">Staff</label>
+          </div>
+          <button type="submit" class="btn btn-danger btn-block mt-2">Submit</button>
         </form>
       </div>
     </div>
