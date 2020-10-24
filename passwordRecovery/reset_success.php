c63297ec2396539e512c03fa0a1aec2f6dccd0ef
Chege Bryan <cbbryan8@gmail.com>
2020-10-24 15:28:15 +0300

    Add reset success page

---
+++ passwordRecovery/reset_success.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <?php include '../head.php'; ?>
+    <title>Password reset success</title>
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-expand navbar-light bg-white mb-4 shadow">
+
+      <a class="navbar-brand text-secondary" href="../index.php">CCM</a>
+
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link text-secondary" href="../index.php">Home</a>
+        </li>
+      </ul>
+    </nav>
+
+
+    <div class="container d-flex justify-content-center mt-5">
+      <div class="card p-4 shadow" style="width: 400px;">
+        <h5 class="text-success card-title">Password reset Successfully</h5>
+        <p class="card-text">Your password reset was successfull.</p>
+        <p class="card-text">You can now login back to your account.</p>
+      </div>
+    </div>
+  </body>
+
+</html>
