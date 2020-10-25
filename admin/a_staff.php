a7420a1819bd0001103a4e71ce43a2d17c0d603f
Chege Bryan <cbbryan8@gmail.com>
2020-10-25 14:00:39 +0300

    Add registred staff download button

--- admin/a_staff.php
+++ admin/a_staff.php
@@ -16,38 +16,41 @@ require_once '../includes/config.php';
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-  <title>Approved Staff Profiles</title>
-  <?php include '../head.php'; ?>
-  <link rel="stylesheet" href="../css/dashboard.css">
-</head>
+  <head>
+    <title>Approved Staff Profiles</title>
+    <?php include '../head.php'; ?>
+    <link rel="stylesheet" href="../css/dashboard.css">
+  </head>
 
-<body>
+  <body>
 
-  <div class="d-flex">
+    <div class="d-flex">
 
-    <?php include 'menu.php'; ?>
-    <!-- Begin Page Content -->
-    <div class="container-fluid pt-9">
+      <?php include 'menu.php'; ?>
+      <!-- Begin Page Content -->
+      <div class="container-fluid pt-9">
 
-      <!-- Page Heading -->
-      <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-secondary">Staff Profiles</h1>
-      </div>
+        <!-- Page Heading -->
+        <div class="d-sm-flex align-items-center justify-content-between mb-4">
+          <h1 class="h3 mb-0 text-secondary">Staff Profiles</h1>
+        </div>
 
-      <?php include 'summary.php'; ?>
+        <?php include 'summary.php'; ?>
 
-      <div class="row">
+        <div class="row">
 
-        <!-- Area Chart -->
-        <div class="col-xl-8 col-lg-7">
-          <div class="card shadow mb-4">
-            <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-              <h6 class="m-0 font-weight-bold text-primary">Approved Staff</h6>
-            </div>
+          <!-- Area Chart -->
+          <div class="col-xl-8 col-lg-7">
+            <div class="card shadow mb-4">
+              <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                <h6 class="m-0 font-weight-bold text-primary">Approved Staff</h6>
+                <a href="../reports/registered_staff.php" class="btn btn-sm btn-primary pull-right" target="_blank"
+                   download><i class="fa fa-download fa-fw"></i>Generate
+                  Report</a>
+              </div>
 
-            <div class="card-body">
-              <?php
+              <div class="card-body">
+                <?php
               $sql = "SELECT ccm_staff.id As staff, fullname, username\n"
 
                 . "FROM ccm_staff \n"
@@ -97,21 +100,21 @@ require_once '../includes/config.php';
               }
               $stmt->close();
               ?>
+              </div>
             </div>
           </div>
-        </div>
 
-        <!-- Pie Chart -->
-        <div class="col-xl-4 col-lg-5">
-          <div class="card shadow mb-4">
-            <!-- Card Header - Dropdown -->
-            <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-              <h6 class="m-0 font-weight-bold text-primary">Staff:
-                <?php echo isset($_GET['name']) ? $_GET['name'] : ""; ?> Activities</h6>
-            </div>
-            <!-- Card Body -->
-            <div class="card-body">
-              <?php
+          <!-- Pie Chart -->
+          <div class="col-xl-4 col-lg-5">
+            <div class="card shadow mb-4">
+              <!-- Card Header - Dropdown -->
+              <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                <h6 class="m-0 font-weight-bold text-primary">Staff:
+                  <?php echo isset($_GET['name']) ? $_GET['name'] : ""; ?> Activities</h6>
+              </div>
+              <!-- Card Body -->
+              <div class="card-body">
+                <?php
               $sql = "SELECT COUNT(ccm_farm_input_payments.id) FROM ccm_farm_input_payments WHERE ccm_farm_input_payments.staff = ?\n"
 
                 . "UNION ALL\n"
@@ -182,21 +185,21 @@ require_once '../includes/config.php';
                 echo "<p class='card-text text-muted'>Select the Staff view activities.</p>";
               }
               ?>
+              </div>
             </div>
           </div>
         </div>
+        <!-- /.container-fluid -->
+
       </div>
-      <!-- /.container-fluid -->
-
-    </div>
-    <!-- Logout Modal-->
-    <?php include '../logout_modal.php'; ?>
+      <!-- Logout Modal-->
+      <?php include '../logout_modal.php'; ?>
 
 
-    <script src="../assets/js/jquery.min.js"></script>
+      <script src="../assets/js/jquery.min.js"></script>
 
-    <script src="../assets/js/popper.min.js"></script>
-    <script src="../assets/bootstrap-4.4.1-dist/js/bootstrap.min.js"></script>
-</body>
+      <script src="../assets/js/popper.min.js"></script>
+      <script src="../assets/bootstrap-4.4.1-dist/js/bootstrap.min.js"></script>
+  </body>
 
 </html>
